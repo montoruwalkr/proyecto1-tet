4b1d5d75dc273c5785905433209cd481077c4ae1
Alex <montoya19981223@outlook.com>
2019-02-09 19:49:06 -0500

    Database 23

--- db/migrate/20190210004426_add_coordinates_to_routes.rb
+++ db/migrate/20190210004426_add_coordinates_to_routes.rb
@@ -1,5 +1,5 @@
 class AddCoordinatesToRoutes < ActiveRecord::Migration[5.1]
   def change
-    add_column :routes, :coordinates, :string, array: true, default: []
+    add_column :routes, :coordinates, :string
   end
 end
